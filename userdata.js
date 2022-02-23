726b4f147afc8deaab0c4db1f5239faa02bc1710
Lars <l.gessner@posteo.de>
2022-02-23 13:20:22 +0100

    add flexible user fetching

--- userdata.js
+++ userdata.js
@@ -1,36 +1,48 @@
 import fetch from 'node-fetch';
 import https from 'https';
 
-const username = "larsgessner"
-const url = `https://teamtreehouse.com/${username}.json`
+function getProfile(username) {
+    const url = `https://teamtreehouse.com/${username}.json`
+    useFetch(url)
+    useHttps(url)
+}
+
 
 /* 1. Using fetch request */
+function useFetch(url) {
 
-fetch(url)
+    fetch(url)
     .then(response => {
         console.log("Fetch Status:", response.status)
         return response
     })
     .then(response => response.json())
     .then(user => printData(user.name, user.badges.length, user.points.total))
+}
 
 
 /* 2. Using HTTPs request */
+function useHttps(url) {
 
-https.get(url, (response) => {
-    console.log("HTTPs Status:", response.statusCode)
-    let body = ""
-    response.on("data", data => {
-        body += data.toString();
+    https.get(url, (response) => {
+        console.log("HTTPs Status:", response.statusCode)
+        let body = ""
+        response.on("data", data => {
+            body += data.toString();
+        })
+    
+        response.on("end", () => {
+            const user = JSON.parse(body)
+            printData(user.name, user.badges.length, user.points.total)
+        })
     })
-
-    response.on("end", () => {
-        const user = JSON.parse(body)
-        printData(user.name, user.badges.length, user.points.total)
-    })
-})
+}
 
 
 function printData(username, badges, points) {
     console.log(`${username} has ${badges} badges with ${points} points!`)
 }
+
+const users = ["chalkers", "alenaholligan", "larsgessner"]
+users.forEach(getProfile)
+
